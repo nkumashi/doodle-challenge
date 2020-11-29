f6671cb87f9369896ff4304b811198a17556a0d4
Kumashi <naveen.kumashi@dxc.com>
2020-11-29 15:33:15 +0100

    Added server starter project

---
+++ chat-demo-server/src/main/java/com/chat/challenge/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.chat.challenge.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {		
+	
+	@GetMapping("/home")
+	public String home() {		
+	 	return "Hello from Server!";
+	}
+}
---
+++ chat-demo-server/src/test/java/com/chat/challenge/ChatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
